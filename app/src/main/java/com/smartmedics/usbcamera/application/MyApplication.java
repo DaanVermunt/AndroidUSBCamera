457237f25437c336a711a5510ab29010c3294f6a
Daan Vermunt <daan@protium.digital>
2020-02-25 16:47:35 +0100

    rename package

---
+++ app/src/main/java/com/smartmedics/usbcamera/application/MyApplication.java
@@ -0,0 +1,23 @@
+package com.smartmedics.usbcamera.application;
+
+import android.app.Application;
+
+import com.smartmedics.usbcamera.utils.CrashHandler;
+
+/**application class
+ *
+ * Created by jianddongguo on 2017/7/20.
+ */
+
+public class MyApplication extends Application {
+    private CrashHandler mCrashHandler;
+    // File Directory in sd card
+    public static final String DIRECTORY_NAME = "USBCamera";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mCrashHandler = CrashHandler.getInstance();
+        mCrashHandler.init(getApplicationContext(), getClass());
+    }
+}
